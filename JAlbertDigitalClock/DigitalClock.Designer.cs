6a97fc08d33276388c1e49231951374b5a6c05af
Jennifer Albert <jennifer.jalbert3@gmail.com>
2024-04-29 17:38:35 -0400

    First time using forms in Visual Studio. I made a simple digital clock.

---
+++ JAlbertDigitalClock/DigitalClock.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace JAlbertDigitalClock
+{
+    partial class DigitalClock
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.clockTimer = new System.Windows.Forms.Timer(this.components);
+            this.clockLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // clockTimer
+            // 
+            this.clockTimer.Interval = 1;
+            this.clockTimer.Tick += new System.EventHandler(this.clockTimer_Tick);
+            // 
+            // clockLabel
+            // 
+            this.clockLabel.AutoSize = true;
+            this.clockLabel.BackColor = System.Drawing.Color.Black;
+            this.clockLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 90F);
+            this.clockLabel.Location = new System.Drawing.Point(132, 125);
+            this.clockLabel.Name = "clockLabel";
+            this.clockLabel.Size = new System.Drawing.Size(654, 170);
+            this.clockLabel.TabIndex = 0;
+            this.clockLabel.Text = "00:00:00";
+            // 
+            // DigitalClock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(935, 450);
+            this.Controls.Add(this.clockLabel);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "DigitalClock";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Digital Clock";
+            this.Load += new System.EventHandler(this.DigitalClock_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer clockTimer;
+        private System.Windows.Forms.Label clockLabel;
+    }
+}
+
---
+++ JAlbertDigitalClock/DigitalClock.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JAlbertDigitalClock
+{
+    public partial class DigitalClock : Form
+    {
+        public DigitalClock()
+        {
+            InitializeComponent();
+        }
+
+        private void clockTimer_Tick(object sender, EventArgs e)
+        {
+            clockLabel.Text = DateTime.Now.ToString("hh:mm:ss");
+        }
+
+        private void DigitalClock_Load_1(object sender, EventArgs e)
+        {
+            clockTimer.Start();
+        }
+    }
+}
